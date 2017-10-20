dbf838c78b61efbec56a59a619bb4df78f4e12fe
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2017-10-20 16:42:14 -0700

    Add files via upload

---
+++ Manual_Control/README.md
@@ -0,0 +1,20 @@
+
+
+# Manual Control Guide
+
+## Install
+	- Open the console & go to the directory you want your code to be
+	- do the following:
+		npm install ar-drone
+		npm install keypress
+	
+	
+## Controls
+	l = launch and land
+	w,a,s,d: to move forward,left,back,right;
+	q,e: to turn left or right
+	space: to stop all movement
+	j: to move vertically up
+	k: to move vertically down
+	ctrl+c : to terminate the program // This is currently buggy
+	
---
+++ Manual_Control/manual_control_v0_11.js
@@ -0,0 +1,76 @@
+
+var arDrone = require('ar-drone');
+var client = arDrone.createClient();
+var keypress = require('keypress');
+var flight_state = false;
+// make `process.stdin` begin emitting "keypress" events 
+keypress(process.stdin);
+ 
+// listen for the "keypress" event 
+process.stdin.on('keypress', function (ch, key) {
+  //console.log('got "keypress"', key);
+  //console.log('got "key.name"', key.name);
+  //client.stop();
+  
+  //console.log('Stop');
+  if (key.name == 'space') {
+	  console.log('Stop');
+	  client.stop();
+  }
+  if (key.name == 'w') {
+	console.log('Go forward');
+	client.front(0.10);
+  }
+  if (key.name == 'a') {
+	console.log('Go left');
+	client.left(0.10);
+  }
+  if (key.name == 'd') {
+	console.log('Go right');
+	client.right(0.10);
+  }
+  if (key.name == 's') {
+	console.log('Go down');
+	client.back(0.10)
+  }
+  if (key.name == 'k') {
+	console.log('Go down');
+	client.down(0.10);
+  }
+  if (key.name == 'j') {
+	console.log('Go up');
+	client.up(0.10);
+  }
+  if (key.name == "e") {
+	console.log('Turn left');
+	client.clockwise(0.10);
+  }
+  if (key.name == 'q') {
+	console.log('Go right');
+	client.counterClockwise(0.10)
+  }
+  if (key.name == 'l') {
+	if (flight_state == false) {
+		client.takeoff();
+		flight_state = true;
+		console.log('Lift off flight_state =',flight_state);
+	}	
+	else {
+		client.land();
+		flight_state = false;
+		console.log('Landing flight_state =',flight_state);
+	}
+  }
+  if (key && key.ctrl && key.name == 'c') {
+	if (flight_state == true) {
+		client.land();
+		flight_state = false;
+	}
+	console.log('End');
+    process.stdin.pause();
+  }
+  
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
