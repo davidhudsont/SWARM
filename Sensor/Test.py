1836d7ddd48b38be02225999ab878e5244abc0fe
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2017-11-18 16:19:19 -0800

    Update Test.py

--- Sensor/Test.py
+++ Sensor/Test.py
@@ -1,8 +1,7 @@
 import serial
 import time
 
-ser = serial.Serial("/dev/ttyS0',57600) # Open up a connection with serial
-	
+ser = serial.Serial("/dev/ttyS0',57600) # Open up a connection with serial	
 	    
 while True:
 	print(ser.readline()) # read a line until the \n character
