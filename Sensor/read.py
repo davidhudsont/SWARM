0fc6af5fceb67125163e61b465765a73d710b916
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2017-11-18 16:23:06 -0800

    Update read.py

--- Sensor/read.py
+++ Sensor/read.py
@@ -2,7 +2,7 @@ import serial
 import time
 
 
-ser = serial.Serial("/dev/ttyS0',57600) # Open up a connection with MCU
+ser = serial.Serial('/dev/ttyS0',57600) # Open up a connection with MCU
 
 while True:
         print(ser.readline()) # read a line until the \n character
